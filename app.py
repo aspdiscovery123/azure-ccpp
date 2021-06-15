07b90567dc75a1dbf0e0ca0ac9cc414acdaa1103
aspdiscovery123 <aspdiscovery@outlook.com>
2021-06-15 11:13:20 +0530

    'asfsf'

---
+++ app.py
@@ -0,0 +1,14 @@
+
+from flask import Flask,request
+import joblib
+import json
+model=joblib.load(r'linear-model.pkl')
+app=Flask(__name__)
+@app.route('/',methods=['POST'])
+
+def abcd():
+    data=request.get_json(force=True)
+    data=data['test']
+    out=model.predict(data)
+    return str(out)
+
